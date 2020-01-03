43f298b46182f4104f6f6df911df97f083810325
Marcos Paulo <marcosp.mail@gmail.com>
2020-01-03 13:43:59 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+
+import api from './services/api';
 
 const App: React.FC = () => {
+  let sessionRequestAccessKey: string;
+  let sessionRegisterAccessKey: string;
+
+
+  useEffect(() => {
+    async function sessionRequest(): Promise<string> {
+      const { data } = await api.post('/auth/session-request', {
+        "system_api_key": '505763d6-4202-4b05-9efc-93b366939bcf'
+      })
+
+      console.log(data.data.access_key);
+
+      return data.data.access_key;
+    }
+
+    async function sessionRegister(): Promise<string> {
+      var config = {
+        headers: { 'Authorization': "bearer " + sessionRequestAccessKey }
+      };
+
+      console.log(config);
+
+      const { data } = await api.post('/auth/session-register', {
+        "organization_user": { "email": "usuario-test@adopets.com", "password": "123123" }
+      },
+        config
+      )
+
+      console.log(data);
+
+      return data;
+    }
+
+    async function init() {
+      sessionRequestAccessKey = await sessionRequest();
+      sessionRegisterAccessKey = await sessionRegister();
+
+      console.log(sessionRegisterAccessKey);
+    }
+
+    init();
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
